15353a422c776df15a2961d6946c3becc3ada032
busma13 <peterluitjens@gmail.com>
2022-06-30 20:09:07 -0700

    update CA tax rate, create footer.ph, header-pt1.php, header-pt2.php. Create admin folder, convert some html files to php. Add headers and footers to php files.

---
+++ footer.php
@@ -0,0 +1,149 @@
+<!-- footer -->
+<footer class="light-footer">
+    <div class="widget-area">
+        <div class="container">
+            <div class="row">
+
+                <!--<div class="col-sm-6 col-md-4 widget">
+                    <div class="about-widget">
+                        <div class="widget-title-image">
+                            <img src="images/logo.png" alt="">
+                        </div>
+                        <p>Praesent sed congue ipsum. Nullam tempus ornare est, non aliquet velit tincidunt elementum. Nulla at risus ut felis eleifend. Nulla non lacinia. Integer est lacus, ultricies sed feugiat id, maximus nec.</p>
+                    </div><! -- / about-widget -->
+                <!--</div><! -- / widget -->
+                
+
+                <div class="col-sm-6 col-md-2 widget">
+                    <div class="widget-title">
+                        <h4>Doorbells</h4>
+                    </div>
+                    <div class="link-widget">
+                        <div class="info">
+                            <a href="doorbells.php">All</a>
+                        </div>
+                        <div class="info">
+                            <a href="doorbells.php?category=beachy">Beachy</a>
+                        </div>
+                        <div class="info">
+                            <a href="doorbells.php?category=contemporary">Contemporary</a>
+                        </div>
+                        <div class="info">
+                            <a href="doorbells.php?category=dog">Dog Lovers</a>
+                        </div>
+                        <div class="info">
+                            <a href="doorbells.php?category=animals">Animals</a>
+                        </div>
+                        <div class="info">
+                            <a href="doorbells.php?category=petroglyphs">Petroglyph</a>
+                        </div>
+                        <div class="info">
+                            <a href="doorbells.php?category=plants">Plants</a>
+                        </div>
+                        <div class="info">
+                            <a href="doorbells.php?category=southwest">Southwest</a>
+                        </div>
+                        <div class="info">
+                            <a href="doorbells.php?category=misc">Miscellaneous</a>
+                        </div>
+                    </div>
+                </div><!-- / widget -->
+                <!-- / first widget -->
+                <div class="col-sm-6 col-md-2 widget">
+                    <div class="widget-title">
+                        <h4>Miscellaneous</h4>
+                    </div>
+                    <div class="link-widget">
+                        <div class="info">
+                            <a href="art.html">Artwork</a>
+                        </div>
+                        <div class="info">
+                            <a href="fan-pulls.html">Ceiling Fan Pulls</a>
+                        </div>
+                        <div class="info">
+                            <a href="air-plant-holders.html">Air Plant Holders</a>
+                        </div>
+                        <div class="info">
+                            <a href="custom-orders.html">Custom Orders</a>
+                        </div>
+                        <!-- <div class="info">
+                            <a href="#x">Animals</a>
+                        </div>
+                        
+                        <div class="info">
+                            <a href="#x">Plants</a>
+                        </div>
+                        <div class="info">
+                            <a href="#x">Traditional</a>
+                        </div> -->
+                    </div>
+                </div><!-- / widget -->
+                <!-- / second widget -->
+
+                <div class="col-sm-6 col-md-2 widget">
+                    <div class="widget-title">
+                        <h4>Support</h4>
+                    </div>
+                    <div class="link-widget">
+                        <div class="info">
+                            <a href="instructions.html">Instructions</a>
+                        </div>
+                        <!-- <div class="info">
+                            <a href="#x">Shipping & Return</a>
+                        </div> -->
+                        <div class="info">
+                            <a href="faq.html">F.A.Q</a>
+                        </div>
+                        <div class="info">
+                            <a href="schedule.html">Show Schedule</a>
+                        </div>
+                        <div class="info">
+                            <a href="contact.html">Contact</a>
+                        </div>
+                    </div>
+                </div><!-- / widget -->
+                <!-- / third widget -->
+
+                <div class="col-sm-6 col-md-4 widget">
+                    <div class="widget-title">
+                        <h4>Get in Touch</h4>
+                    </div>
+                    <div class="contact-widget">
+                        <div class="info">
+                            <p><i class="lnr lnr-map-marker"></i><span>11885 Rocoso Road<br> Lakeside, CA 92040-1035</span></p>
+                        </div>
+                        <div class="info">
+                            <a href="tel:6194671176"><i class="lnr lnr-phone-handset"></i><span>619-467-1176</span></a>
+                        </div>
+                        <div class="info">
+                            <a href="mailto:info@cicadaceramics.com"><i class="lnr lnr-envelope"></i><span>info@cicadaceramics.com</span></a>
+                        </div>
+                        <!-- <div class="info">
+                            <p class="social pull-left">
+                                <a class="no-margin" href="#"><i class="fa fa-facebook"></i></a>
+                                <a href="#"><i class="fa fa-twitter"></i></a>
+                                <a href="#"><i class="fa fa-google-plus"></i></a>
+                                <a href="#"><i class="fa fa-pinterest"></i></a>
+                                <a href="#"><i class="fa fa-linkedin"></i></a>
+                                <a href="#"><i class="fa fa-dribbble"></i></a>
+                            </p>
+                        </div> -->
+                    </div><!-- / contact-widget -->
+                </div><!-- / widget -->
+                <!-- / fourth widget -->
+
+            </div><!-- / row -->
+        </div><!-- / container -->
+    </div><!-- / widget-area -->
+    <div class="footer-info">
+        <div class="container">
+                <div class="pull-left">
+                    <p>© 2022 - <strong>Cicada Ceramics</strong> </p>
+                </div>
+                <span class="pull-right">
+                    <span>???</span>
+                </span>
+        </div><!-- / container -->
+    </div><!-- / footer-info -->
+</footer>
+<!-- / footer -->
