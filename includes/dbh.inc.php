e1ffc60b6ba7543c107aca6bb6b894ae6f6f2500
busma13 <peterluitjens@gmail.com>
2022-07-01 19:40:07 -0700

    change dbh.inc.php

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -1,4 +1,4 @@
-<?php
+<!-- < ?php
 // database handler includes folder = dbh.inc.php
 
 $dbServername = "localhost"; //needs to point to actual online server 
@@ -6,4 +6,15 @@ $dbUsername = "root"; //will be different for online server
 $dbPassword = ""; //xampp has no default pw
 $dbName = "doorbell_designs";
 
-$conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
+$conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName); -->
+
+<?php
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+$server = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$db = substr($url["path"], 1);
+
+$conn = new mysqli($server, $username, $password, $db);
+?>
