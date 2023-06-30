278e5e078975638e675b3ec21744cfa513d7de9f
busma13 <peterluitjens@gmail.com>
2023-06-30 06:29:17 -0700

    Test file needed to be capitalized.

---
+++ tests/Unit/Order/SetItemNameTest.php
@@ -0,0 +1,38 @@
+<?php
+
+
+namespace Tests\Unit\Order;
+
+use Tests\Support\UnitTester;
+
+require_once __DIR__. '/../../../includes.order.inc.php';
+
+class SetItemNameTest extends \Codeception\Test\Unit
+{
+
+    protected UnitTester $tester;
+
+    protected function _before()
+    {
+    }
+
+    // tests
+    public function testSetItemNameFunction()
+    {
+        $optionsObject = (object) [
+            '17' => 2,
+            'optionsIDString' => '2',
+            'optionsPairStrings' => [
+                (object) [
+                   'stringKey' => 'Color:',
+                   'stringValue' => 'Blue Azure'
+                ]
+            ]
+        ];
+        $itemObj = (object) [
+            'itemNameString' => 'Air Plant Cradle',
+            'options' => $optionsObject
+        ];
+        $this->assertTrue(setItemName($itemObj) == 'Air Plant Cradle, Color: Blue Azure');
+    }
+}
